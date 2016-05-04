de515c943b1dbee7620e52e3d08b2f9585b157e2
Btnisme -github data <Btnisme123@gmail.com>
2016-05-04 11:21:05 +0700

    update cached data for history screen

--- RSSFeed/app/src/main/java/com/framgia/rssfeed/fragment/HistoryFragment.java
+++ RSSFeed/app/src/main/java/com/framgia/rssfeed/fragment/HistoryFragment.java
@@ -1,21 +1,26 @@
 package com.framgia.rssfeed.fragment;
 
+import android.content.Context;
+import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
 
 import com.framgia.rssfeed.ListViewItemDecoration;
+import com.framgia.rssfeed.OnRecyclerViewItemClickListener;
 import com.framgia.rssfeed.R;
 import com.framgia.rssfeed.adapter.ListNewsAdapter;
 import com.framgia.rssfeed.base.BaseFragment;
+import com.framgia.rssfeed.base.Constants;
+import com.framgia.rssfeed.bean.News;
 import com.framgia.rssfeed.data.local.DatabaseHandler;
 
 
-public class HistoryFragment extends BaseFragment {
+public class HistoryFragment extends BaseFragment implements OnRecyclerViewItemClickListener {
 
     private RecyclerView mRecyclerView;
-    private ListNewsAdapter mListNewsAdapter;
-    private ListViewItemDecoration mListViewItemDecoration;
+    private ListNewsAdapter mListHistoryAdapter;
+
 
     @Override
     protected int getFragmentLayoutId() {
@@ -25,24 +30,37 @@ public class HistoryFragment extends BaseFragment {
     @Override
     protected void onCreateContentView(View rootView) {
         findView(rootView);
+        setupRecyclerView(getContext());
     }
 
     public void findView(View view) {
         mRecyclerView = (RecyclerView) view.findViewById(R.id.recycler_news);
-        setupRecyclerView();
     }
 
-    public void setupRecyclerView() {
-        mListViewItemDecoration = new ListViewItemDecoration(getActivity());
-        mRecyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+    public void setupRecyclerView(Context context) {
+        ListViewItemDecoration mListViewItemDecoration= new ListViewItemDecoration(context);;
+        mRecyclerView.setLayoutManager(new LinearLayoutManager(context));
         mRecyclerView.addItemDecoration(mListViewItemDecoration);
-        mListNewsAdapter = new ListNewsAdapter(getActivity(), mRecyclerView.getLayoutManager());
-        mListNewsAdapter.addItems(DatabaseHandler.getInstance(getActivity()).getHistoryNews());
-        mRecyclerView.setAdapter(mListNewsAdapter);
+        mListHistoryAdapter = new ListNewsAdapter(context, mRecyclerView.getLayoutManager());
+        mListHistoryAdapter.addItems(DatabaseHandler.getInstance(context).getHistoryNews());
+        mListHistoryAdapter.setOnRecyclerViewItemClickListener(this);
+        mRecyclerView.setAdapter(mListHistoryAdapter);
     }
 
     @Override
     protected boolean enableBackButton() {
         return false;
+    }
+
+
+    @Override
+    public void onItemClickListener(int position) {
+        News news = mListHistoryAdapter.getItem(position);
+        DatabaseHandler.getInstance(getActivity()).insertNewsInfo(news);
+        Bundle bundle = new Bundle();
+        bundle.putString(Constants.BUNDLE_URL, news.getLink());
+        ShowDetailFragment fragment = new ShowDetailFragment();
+        fragment.setArguments(bundle);
+        getBaseActivity().replaceFragment(fragment, ListNewsFragment.TAG_LIST_NEWS_FRAGMENT);
     }
 }
